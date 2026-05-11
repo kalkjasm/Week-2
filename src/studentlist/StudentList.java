dc66e5ad764ae2cfadaa113729070e8ddc33e2b4
kalkjasm <kalkjasm@sheridancollege.ca>
2026-05-11 10:47:31 -0400

    student version 2 (changed studentList size)

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -23,9 +23,9 @@ public class StudentList {
         Student s2=new Student();
         s2.setName("John");
         s2.setSid(2);
-        Student studentList[] =new Student[2];
+        Student studentList[] =new Student[1];
         studentList[0]=s1;
-        studentList[1]=(s2);
+//        studentList[1]=(s2);
         for(int i=0; i<studentList.length; i++){
             System.out.println(studentList[i].getName()+" has I'd: "+studentList[i].getSid());
         }
